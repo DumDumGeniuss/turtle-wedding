aae9e5ecee6b61aa411118efb58e25b949524ec9
DumDumGeniuss <messi_yang@syntrontech.com>
2016-09-05 00:29:12 +0800

    fix css

--- src/components/displayPhotos/DisplayPhotos.jsx
+++ src/components/displayPhotos/DisplayPhotos.jsx
@@ -21,7 +21,7 @@ export default class DisplayPhotos extends React.Component {
 					{
 						photos.map( (item) => {
 							return (
-								<SimplePhotoBox photo={item} photoWidth={'200px'} photoHeight={"200px"}/>
+								<SimplePhotoBox photo={item} photoWidth={'150px'} photoHeight={"150px"}/>
 							)
 						})
 					}

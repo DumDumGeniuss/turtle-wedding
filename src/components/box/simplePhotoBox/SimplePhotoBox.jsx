c200caae2989f14047488a7068515c9616f15b3b
DumDumGeniuss <messi_yang@syntrontech.com>
2016-09-05 01:05:45 +0800

    make photos smooth

--- src/components/box/simplePhotoBox/SimplePhotoBox.jsx
+++ src/components/box/simplePhotoBox/SimplePhotoBox.jsx
@@ -10,12 +10,9 @@ export default class SimplePhotoBox extends React.Component {
 		const self = this
 		let props = self.props  
 		let photo = props.photo
-		let photoWidth = props.photoWidth
-		let photoHeight = props.photoHeight
-		console.log(photoHeight)
 
 		return (
-			<div className="SimplePhotoBox-mainArea" style={ {width: photoWidth, height: photoHeight} }>
+			<div className="SimplePhotoBox-mainArea">
 				<img className="SimplePhotoBox-photo" src={config.apiUrl + '/files/photos/' + photo.fileName}/>
 			</div>
 		)
